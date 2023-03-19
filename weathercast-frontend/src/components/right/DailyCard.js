bdd4ab118126628c0220e65b35dadb7ad3694796
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-03-19 14:31:58 +0800

    Empty cards if no data has been searched

--- weathercast-frontend/src/components/right/DailyCard.js
+++ weathercast-frontend/src/components/right/DailyCard.js
@@ -3,16 +3,20 @@ import React from 'react'
 export const DailyCard = ({index, data}) => {
   return (
     <div className="daily-card" key={index}>
-        <div className="daily-card-temperature">
-            {data?.day?.avgtemp_c + "°C" || "28°C"}
-        </div>
-        <div>
-            <div className="daily-card-status">{data?.day?.condition?.text || "Rain"}</div>
+        {data?.day && 
+        <>
+            <div className="daily-card-temperature">
+                {data?.day?.avgtemp_c + "°C" || "28°C"}
+            </div>
+            <div>
+                <div className="daily-card-status">{data?.day?.condition?.text || "Rain"}</div>
             <div></div>
-        </div>
-        <div className="daily-card-date">
-            {data?.date || "Tue 28, July 20"}
-        </div>
+            </div>
+            <div className="daily-card-date">
+                {data?.date || "Tue 28, July 20"}
+            </div>
+        </>
+        }
     </div>
   )
 }
--- weathercast-frontend/src/components/right/TodayCard.js
+++ weathercast-frontend/src/components/right/TodayCard.js
@@ -6,21 +6,26 @@ export const TodayCard = () => {
 
   return (
     <div className="today-card">
-      <div>
-        <div className="today-card-temperature">{data?.current?.temp_c + "°C" || "32°C"}</div>
-        <div className="today-card-status">{ data?.current?.condition?.text || "Sunny"}</div>
-        <div className="today-card-date">{data?.current?.last_updated || "Monday 27, July 20"}</div>
-      </div>
-      <div className="vertical-border"></div>
-      <div className="today-card-right">
-        <div>Real Feel: {data?.current?.feelslike_c + "°C" || ""}</div>
-        <div>Humidity: {data?.current?.humidity + "%" || ""}</div>
-        <div>UV Index: {data?.current?.uv || ""}</div>
-        <div>Wind: {data?.current?.wind_dir + " " + data?.current?.wind_kph + " kph" || ""} </div>
-        <div>Visibility: {data?.current?.vis_km + " km" || ""}</div>
-        <div>Cloud Cover: {data?.current?.cloud + "%" || ""}</div>
-      </div>
       
+        {data?.current && 
+        <>
+          <div>
+            <div className="today-card-temperature">{data.current.temp_c + "°C" || ""}</div>
+            <div className="today-card-status">{ data?.current?.condition?.text || ""}</div>
+            <div className="today-card-date">{data?.current?.last_updated || ""}</div>
+          </div>
+          <div className="vertical-border"></div>
+
+          <div className="today-card-right">
+            <div>Real Feel: {data?.current?.feelslike_c + "°C" || ""}</div>
+            <div>Humidity: {data?.current?.humidity + "%" || ""}</div>
+            <div>UV Index: {data?.current?.uv || ""}</div>
+            <div>Wind: {data?.current?.wind_dir + " " + data?.current?.wind_kph + " kph" || ""} </div>
+            <div>Visibility: {data?.current?.vis_km + " km" || ""}</div>
+            <div>Cloud Cover: {data?.current?.cloud + "%" || ""}</div>
+          </div>
+        </>
+        }
     </div>
   )
 }
