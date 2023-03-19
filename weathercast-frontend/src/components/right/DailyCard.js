c178152518952f2c91b6bac275d2971f643aa58f
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-03-19 09:59:18 +0800

    Add basic CSS styling to daily card

--- weathercast-frontend/src/components/right/DailyCard.js
+++ weathercast-frontend/src/components/right/DailyCard.js
@@ -3,14 +3,14 @@ import React from 'react'
 export const DailyCard = () => {
   return (
     <div className="daily-card">
-        <div>
+        <div className="daily-card-temperature">
             28°C
         </div>
         <div>
-            <div>Rain</div>
+            <div className="daily-card-status">Rain</div>
             <div></div>
         </div>
-        <div>
+        <div className="daily-card-date">
             Tue 28, July 20
         </div>
     </div>
