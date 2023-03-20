5b4acd05aa295e25daa3358f96a01e7c15b281c4
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-03-20 12:54:12 +0800

    Add basic graph functionality

--- weathercast-frontend/src/components/details/DetailsGraph.js
+++ weathercast-frontend/src/components/details/DetailsGraph.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, Label } from 'recharts';
 
 export const DetailsGraph = ({data , page }) => {
 
     const graphData = data.forecast.forecastday[page].hour
 
   return (
-    <div className="details-graph-container">
-        <LineChart width={730} height={220} data={graphData}>
-            <XAxis dataKey="time"/>
-            <YAxis dataKey="temp_c"/>
-            <Line dataKey="temp_c"/>
-        </LineChart>
+    <div className="details-graph-card">
+        <div className="details-graph-container">
+            <LineChart width={730} height={220} data={graphData}>
+                <XAxis dataKey="time" tickFormatter={(data)=>(
+                    data.slice(11)
+                )}>
+                    {/* <Label value="Time" offset={0} position="bottom"/> */}
+                </XAxis>
+                <YAxis dataKey="temp_c" tickFormatter={(data)=>(data + "°C")}>
+                    {/* <Label value="°C" offset={0} angle={-90} position="center" /> */}
+                </YAxis>
+                <Line dataKey="temp_c"/>
+                <Tooltip formatter={(label)=>(label + "°C")}/>
+            </LineChart>
+        </div>
     </div>
   )
 }
